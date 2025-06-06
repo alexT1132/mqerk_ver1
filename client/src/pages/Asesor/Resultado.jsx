378e650476c2cf377e868048057693da461ca50c
Darian Reyes Romero <darianreyesromero@hotmail.com>
2025-06-06 14:32:45 -0600

    Ajustes en diseno responsive

--- client/src/pages/Asesor/Resultado.jsx
+++ client/src/pages/Asesor/Resultado.jsx
@@ -8,7 +8,7 @@ export const ResultadoAsesor=({area})=>{
     return(
         <>
         <Topbar/>
-        <div className={`flex flex-col gap-10 justify-center items-center my-5 mx-20`}>
+        <div className={`flex flex-col gap-10 justify-center items-center mx-2 my-2 sm:my-5 sm:mx-5 md:mx-10 lg:mx-20`}>
         <h1 className={`font-bold text-center text-2xl`}>MARIANA RODRIGUEZ PEREZ</h1>
 
         <h2 className={`font-semibold text-center`}>
@@ -27,8 +27,8 @@ export const ResultadoAsesor=({area})=>{
         <BtnFuncion funcion={`Iniciar tramite`}/>
         </div>
 
-        <footer className={`mx-40`}>
-            <p className={`text-justify text-sm font-thin`}>✅ ¡Felicidades! Usted ha sido aceptado para el puesto de asesor(a) educativo en el área de {area}.
+        <footer className={``}>
+            <p className={`text-justify text-sm font-semibold sm:text-sm md:text-sm lg:font-thin`}>✅ ¡Felicidades! Usted ha sido aceptado para el puesto de asesor(a) educativo en el área de {area}.
                 los resultados obtenidos reflejan que posee las competencias y valores que buscamos.
                 Nos entusiasma contar con usted en nuestro equipo para impactar positivamente en la vida de nuestros
                 estudiantes
