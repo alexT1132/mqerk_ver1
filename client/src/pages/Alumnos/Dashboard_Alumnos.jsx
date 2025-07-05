c5c63c4ba05bdec90d92a5b56e6db1b921b6adf1
MiguelAngelCruzVargas <144273695+MiguelAngelCruzVargas@users.noreply.github.com>
2025-07-05 11:02:55 -0600

    Add files via upload
    contenedor pricipal del  dashboard de alumnos

---
+++ client/src/pages/Alumnos/Dashboard_Alumnos.jsx
@@ -0,0 +1,8 @@
+// src/pages/Alumnos/Dashboard_Alumnos.jsx
+import React from "react";
+import { AlumnoDashboardBundle } from "../../components/AlumnoDashboardBundle.jsx";
+
+// Página principal del dashboard del alumno
+export default function DashboardAlumno() {
+  return <AlumnoDashboardBundle />;
+}
